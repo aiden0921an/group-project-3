3d933a3d9c67f184bd14cb22b1d2e2dc42a92138
aiden0921an <nelsonaiden23@gmail.com>
2024-09-06 08:48:44 -0500

    added in hook for payment

---
+++ client/pages/AboutPage.jsx
@@ -0,0 +1,12 @@
+
+
+
+export default function AboutUs(){
+
+    return (
+      <>
+        <h1>About us:</h1>
+        <p>To do: write up a project summary or something among those lines</p>
+      </>
+    )
+  }
