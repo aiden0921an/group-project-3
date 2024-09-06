26cdf0004b639909b4290622d4a562b9c33be5d0
aiden0921an <nelsonaiden23@gmail.com>
2024-09-06 11:53:21 -0500

    started new pages

---
+++ client/pages/PostPage.jsx
@@ -0,0 +1,14 @@
+
+
+//We will want this page to reroute to the login if the user is not logged in
+
+
+export default function PostPage(){
+
+    return (
+      <>
+        <h1>Private Page</h1>
+        <p>This is an example of a page that would require an authenticated user.</p>
+      </>
+    )
+  }
