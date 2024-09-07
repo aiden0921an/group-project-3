6face39582dc5519209dfdf0f1ac76df168dd5e8
Ben Merchant <benmerch@gmail.com>
2024-09-06 20:03:09 -0500

    Getting there, I think.

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const postRoutes = require("./post.routes");
 const userRoutes = require("./user.routes");
+const paymentRoutes = require("./payment.routes");
 
 router.use("/post", postRoutes);
 router.use("/user", userRoutes);
+router.use("/payment", paymentRoutes);
 
 module.exports = router;
