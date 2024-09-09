dfc324d67d3c1a73d006817dc146ba440737af51
aiden0921an <nelsonaiden23@gmail.com>
2024-09-09 12:13:47 -0500

    oops broke it all

--- client/pages/SavedPage.jsx
+++ client/pages/SavedPage.jsx
@@ -1,52 +1,52 @@
 
 
 
-import {createContext, useContext, useState, useEffect} from "react"
-const sharedContext= () => useContext(sharedContext)
+// import {createContext, useContext, useState, useEffect} from "react"
+// const sharedContext= () => useContext(sharedContext)
 
 
 
-//Bookmarks will initially be saved to and pulled from local storage
-export const ShareProvider = ({children}) => {
-  const [bookmarkedItems, setBookmarkedItems] = useState (() => {
-    const storedBookmarks = JSON.parse(localStorage.getItem(LocalStorageKey));
-    return Array.isArray(storedBookmarks) ? storedBookmarks : []
-  })
-}
+// //Bookmarks will initially be saved to and pulled from local storage
+// export const ShareProvider = ({children}) => {
+//   const [bookmarkedItems, setBookmarkedItems] = useState (() => {
+//     const storedBookmarks = JSON.parse(localStorage.getItem(LocalStorageKey));
+//     return Array.isArray(storedBookmarks) ? storedBookmarks : []
+//   })
+// }
 
-const addBookmark = (post) => {
-  setBookmarkedItems ((prevBookmarks) => [...prevBookmarks, post]);
-}
+// const addBookmark = (post) => {
+//   setBookmarkedItems ((prevBookmarks) => [...prevBookmarks, post]);
+// }
 
-useEffect(() => {
-  try {
-    const storedBookmarks= JSON.parse(localStorage.getItem(LocalStorageKey));
-    if (Array.isArray(storedBookmarks)) {
-      setBookmarkedItems(storedBookmarks)
-    }
-  } catch (err) {
-    console.log("Error getting bookmarks", err)
-  }
-}, [])
+// useEffect(() => {
+//   try {
+//     const storedBookmarks= JSON.parse(localStorage.getItem(LocalStorageKey));
+//     if (Array.isArray(storedBookmarks)) {
+//       setBookmarkedItems(storedBookmarks)
+//     }
+//   } catch (err) {
+//     console.log("Error getting bookmarks", err)
+//   }
+// }, [])
 
-const clearBookmarks= () => {
-  localStorage.removeItem(LocalStorageKey);
-  setBookmarkedItems([]);
-}
+// const clearBookmarks= () => {
+//   localStorage.removeItem(LocalStorageKey);
+//   setBookmarkedItems([]);
+// }
 
-const removeBookmark = (postSlug) => {
-  setBookmarkedItems((prevBookmarks) =>
-    prevBookmarks.filter((post) => post.slug !== postSlug)
-  );
-};
+// const removeBookmark = (postSlug) => {
+//   setBookmarkedItems((prevBookmarks) =>
+//     prevBookmarks.filter((post) => post.slug !== postSlug)
+//   );
+// };
 
-return (
-  <sharedContext.Provider 
-  value ={{bookmarkedItems, addBookmark, removeBookmark, clearBookmarks}}
-  >
-    {children}
-  </sharedContext.Provider>
-)
+// return (
+//   <sharedContext.Provider 
+//   value ={{bookmarkedItems, addBookmark, removeBookmark, clearBookmarks}}
+//   >
+//     {children}
+//   </sharedContext.Provider>
+// )
 
 //We will want this page to reroute to the login if the user is not logged in
 export default function SavedPage(){
