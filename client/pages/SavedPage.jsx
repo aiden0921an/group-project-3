823a8301f8c4138e81191fc77e14848a6e276357
aiden0921an <nelsonaiden23@gmail.com>
2024-09-09 11:10:43 -0500

    started bookmarks

--- client/pages/SavedPage.jsx
+++ client/pages/SavedPage.jsx
@@ -1,9 +1,28 @@
 
 
+
+import {createContext, useContext, useState, useEffect} from "react"
+const sharedContext= () => useContext(sharedContext)
+
+
+
+//Bookmarks will initially be saved to and pulled from local storage
+export const ShareProvider = ({children}) => {
+  const [bookmarkedItems, setBookmarkedItems] = useState (() => {
+    const storedBookmarks = JSON.parse(localStorage.getItem(LocalStorageKey));
+    return Array.isArray(storedBookmarks) ? storedBookmarks : []
+  })
+}
+
+const addBookmark = (post) => {
+  setBookmarkedItems ((prevBookmarks) => [...prevBookmarks, post]);
+}
+
+
+
+
 //We will want this page to reroute to the login if the user is not logged in
-
-
-export default function PostPage(){
+export default function SavedPage(){
 
     return (
       <>
