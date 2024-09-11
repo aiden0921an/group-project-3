25ded2ff8d45a5d1bf8f2df27344897d2c7687a5
Ben Merchant <benmerch@gmail.com>
2024-09-10 21:35:19 -0500

    Adjusted the uploads so it can handle both image and no image posts.

--- client/pages/AboutPage.jsx
+++ client/pages/AboutPage.jsx
@@ -1,16 +1,18 @@
-
-
-
 export default function AboutUs() {
-
   return (
     <>
-      <div class="d-flex flex-column">
+      <div className="d-flex flex-column">
         <h1>About us:</h1>
-        <p>This application was created by Aiden, Ben, Dan, and Noah as part of a coding bootcamp through the University of Minnesota. By using skills we have learned in the cohort, we were able to create a working ecommerce marketplace application that allows users to log in and create posts for sale. </p>
+        <p>
+          This application was created by Aiden, Ben, Dan, and Noah as part of a
+          coding bootcamp through the University of Minnesota. By using skills
+          we have learned in the cohort, we were able to create a working
+          ecommerce marketplace application that allows users to log in and
+          create posts for sale.{" "}
+        </p>
         <h2>Contact Us:</h2>
         <p>fakeemail@email.com</p>
       </div>
     </>
-  )
+  );
 }
